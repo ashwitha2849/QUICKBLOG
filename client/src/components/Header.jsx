ed7a04588d66944ba69071556e2254a66167562c
ashwitha2849 <ashwithat2004@gmail.com>
2025-09-30 21:39:25 +0530

    commit

--- client/src/components/Header.jsx
+++ client/src/components/Header.jsx
@@ -20,14 +20,14 @@ const Header=() =>{
     <div className="mx-8 sm:mx-16 xlmx-24 relative">
       <div className="text-center mt-20 mb-8">
         <div className="inline-flex items-center justify-center gap-4 px-6 py-1.5 mb-4 border border-primary/40 bg-primary/10 rounded-full text-sm text-primary">
-          <p>New: AI frature integrated</p>
+          <p>New: AI feature integrated</p>
           <img src={assets.star_icon} className="w-2.5" alt="" />
         </div>
         <h1
           className="text-3xl sm:text-6xl font font-semibold
                sm:leading-16 text-gray-700"
         >
-          It's AMR's <span className="text-primary">blogging</span> <br />
+          Your Own <span className="text-primary">blogging</span> <br />
           platform
         </h1>
         <p
